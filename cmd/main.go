9f9b212d7e5780becd126d4a8e82b899c0927f01
Parviz Boymurodov <parvizboymurodov@mail.ru>
2020-09-10 10:08:30 +0500

    first version

---
+++ cmd/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"io/ioutil"
+	"log"
+	"nellymarket/cmd/app"
+	"nellymarket/pkg/models"
+	"nellymarket/pkg/services"
+	"net"
+	"net/http"
+	"path/filepath"
+)
+
+var conf models.Config
+
+func init() {
+	data, err := ioutil.ReadFile("./cmd/configs.json")
+	if err != nil {
+		log.Fatalf("Can't read file : %v\n", err)
+	}
+	err = json.Unmarshal(data, &conf)
+	if err != nil {
+		log.Fatalf("Can't unmarshal data : %v\n", err)
+	}
+}
+
+func main() {
+	addr := net.JoinHostPort(conf.Host, conf.Port)
+	start(addr, conf.Dsn)
+}
+
+func start(addr string,dsn string) {
+	router := app.NewExactMux()
+
+	pool, err := pgxpool.Connect(context.Background(), dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	nellyMarket := services.NewNellyMarket(pool)
+
+	server := app.NewServer(
+		pool,
+		router,
+		nellyMarket,
+		filepath.Join("web", "templates"),
+		filepath.Join("web", "assets"),
+	)
+
+	server.InitRoutes()
+
+	nellyMarket.Start()
+
+	panic(http.ListenAndServe(addr,server))
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module nellymarket
+
+go 1.15
+
+require (
+	github.com/jackc/pgx/v4 v4.8.1
+	github.com/shopspring/decimal v1.2.0
+)
